baa691a82eb71ac95c8044f3ff28c25c830dd1fe
black2602 <black2602@gmail.com>
2017-11-29 00:20:21 +0900

    multipart 이미지 업로드 컨트롤러 메소드 추

--- src/main/java/com/blackangel/baskettogether/controller/UserController.java
+++ src/main/java/com/blackangel/baskettogether/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.blackangel.baskettogether.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.io.Writer;
-import java.sql.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,6 +24,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.blackangel.baskettogether.common.domain.MyError;
 import com.blackangel.baskettogether.user.domain.User;
@@ -99,6 +104,52 @@ public class UserController {
 		return null;
 	}
 
+	@RequestMapping(value="/user/photo", method=RequestMethod.POST)
+	public ResponseEntity<String> uploadPhoto(MultipartFile uploadfile) {
+		OutputStream out = null;
+        PrintWriter printWriter = null;
+ 
+        try {
+            // 파일명 얻기
+//        	MultipartFile uploadfile = request.getFile("uploadfile");
+            String fileName = uploadfile.getOriginalFilename();
+            // 파일의 바이트 정보 얻기
+            byte[] bytes = uploadfile.getBytes();
+            // 파일의 저장 경로 얻기
+            String uploadPath = "/Users/kimjeonghun/Develop/storage/basket-together/test.jpg";
+             
+            // 파일 객체 생성
+            File file = new File(uploadPath);
+            // 상위 폴더 존재 여부 확인
+            if (!file.getParentFile().exists()) {
+                // 상위 폴더가 존재 하지 않는 경우 상위 폴더 생성
+                file.getParentFile().mkdirs();
+            }
+             
+            // 파일 아웃풋 스트림 생성
+            out = new FileOutputStream(file);
+            // 파일 아웃풋 스트림에 파일의 바이트 쓰기
+            out.write(bytes);
+             
+            return new ResponseEntity<String>(HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (printWriter != null) {
+                    printWriter.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+         
+        return null;
+	}
+	
 	@ExceptionHandler(DataAccessException.class)
 	@ResponseBody
 	public void handleException(HttpServletRequest req, HttpServletResponse res, Writer writer, DataAccessException ex) {
